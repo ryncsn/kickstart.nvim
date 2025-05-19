aef1bb055cc578725a43d86d54b13de78136e838
Kairui Song <ryncsn@gmail.com>
2025-05-19 15:17:20 +0800

    plugin: lint: add checkpatch.pl
    Need to setup a ~/.local/bin/checkpatch.pl though.
    
    Signed-off-by: Kairui Song <ryncsn@gmail.com>

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,7 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        c = { 'checkpatch' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
