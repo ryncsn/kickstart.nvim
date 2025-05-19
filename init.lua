540c9bc32d0ad356cf19f49ee25881c5288b0fbc
Kairui Song <ryncsn@gmail.com>
2025-05-19 14:10:53 +0800

    plugin: jellybean color scheme
    Signed-off-by: Kairui Song <ryncsn@gmail.com>

--- init.lua
+++ init.lua
@@ -766,29 +766,21 @@ require('lazy').setup({
       signature = { enabled = true },
     },
   },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  { -- PLUGIN: Colorscheme.
+    'WTFox/jellybeans.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      on_colors = function(c)
+        local light_bg = '#ffffff'
+        local dark_bg = '#000000'
+        c.background = vim.o.background == 'light' and light_bg or dark_bg
+      end,
+    },
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'jellybeans'
     end,
   },
-
   { -- PLUGIN: Highlight todo, notes, etc in comments
     'folke/todo-comments.nvim',
     event = 'VimEnter',
