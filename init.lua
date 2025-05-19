190ff03a32927b55b2e5e745b039b2b70ec6141b
Kairui Song <ryncsn@gmail.com>
2025-05-19 13:40:46 +0800

    plugin: sanitize format and comments
    Signed-off-by: Kairui Song <ryncsn@gmail.com>

--- init.lua
+++ init.lua
@@ -150,19 +150,10 @@ local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  { -- PLUGIN: Detect tabstop and shiftwidth automatically
+    'NMAC427/guess-indent.nvim',
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
