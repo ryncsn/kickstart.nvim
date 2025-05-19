15163d48e7df1d74348ffc13544ffc0957bff518
Kairui Song <ryncsn@gmail.com>
2025-05-19 14:28:23 +0800

    config: trim comments for linenumber
    Signed-off-by: Kairui Song <ryncsn@gmail.com>

--- init.lua
+++ init.lua
@@ -15,8 +15,7 @@ vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
+--  No relativenumber
 -- vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
