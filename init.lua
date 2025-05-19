53ab4f919b4b896f4fb11b05734ab51da85716b1
Kairui Song <ryncsn@gmail.com>
2025-05-19 14:12:05 +0800

    plugin: update auto completion and snips
    Signed-off-by: Kairui Song <ryncsn@gmail.com>

--- init.lua
+++ init.lua
@@ -667,8 +667,7 @@ require('lazy').setup({
       },
     },
   },
-
-  { -- Autocompletion
+  { -- PLUGIN: Autocompletion
     'saghen/blink.cmp',
     event = 'VimEnter',
     version = '1.*',
@@ -690,12 +689,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
         opts = {},
       },
