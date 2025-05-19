f0472476d2464d28250aced71bf18f5dbf05ad0a
Kairui Song <ryncsn@gmail.com>
2025-05-20 01:38:12 +0800

    plugin: update rainbow
    Signed-off-by: Kairui Song <ryncsn@gmail.com>

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -5,11 +5,11 @@ return {
     -- See `:help ibl`
     config = function()
       local highlight = {
-        'RainbowRed',
-        'RainbowYellow',
         'RainbowBlue',
-        'RainbowOrange',
         'RainbowGreen',
+        'RainbowYellow',
+        'RainbowOrange',
+        'RainbowRed',
         'RainbowViolet',
         'RainbowCyan',
       }
@@ -18,13 +18,13 @@ return {
       -- create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
-        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
-        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
-        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
-        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
-        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
-        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#313F9F' })
+        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#889339' })
+        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#C5B04B' })
+        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#A17A46' })
+        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#D04C55' })
+        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#B668BD' })
+        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56A6B2' })
       end)
 
       require('ibl').setup { indent = { highlight = highlight } }
