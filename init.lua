84ea89995066a9d8601d4f423a312c38b5c3276c
Kairui Song <ryncsn@gmail.com>
2025-05-29 16:17:12 +0800

    config: disable auto clipboard

--- init.lua
+++ init.lua
@@ -32,9 +32,11 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
+-- NOTE: Not using this so clipboard can be a standalone buffer,
+-- doesn't effect mouse mode.
+-- vim.schedule(function()
+--   vim.o.clipboard = 'unnamedplus'
+-- end)
 
 -- Enable break indent
 vim.o.breakindent = true
