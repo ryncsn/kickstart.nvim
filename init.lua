20b62a0bd8fcfd33fceb54d4f00d28023abf9d80
Kairui Song <ryncsn@gmail.com>
2025-07-29 14:48:14 +0800

    rust: enable analyzer and no auto format

--- init.lua
+++ init.lua
@@ -583,7 +583,7 @@ require('lazy').setup({
         },
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -664,7 +664,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, rust = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
